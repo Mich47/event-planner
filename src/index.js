1a4f44fb496d025840655ac0f39786109f69dcc1
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-08-02 23:44:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <BrowserRouter basename="/event-planner">
       <App />
-    </HashRouter>
-    {/* <BrowserRouter basename="/event-planner"></BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
